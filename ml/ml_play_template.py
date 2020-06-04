29665ab1c78d9c7382d4babc5eadfb5694258a65
Yen900611 <yen900611@gmail.com>
2020-06-04 15:17:41 +0800

    debug

--- ml/ml_play_template.py
+++ ml/ml_play_template.py
@@ -22,8 +22,6 @@ class MLPlay:
             if car["id"]==self.player_no:
                 self.car_vel = car["velocity"]
 
-        print(self.car_pos)
-
         if scene_info["status"] != "ALIVE":
             return "RESET"
 
