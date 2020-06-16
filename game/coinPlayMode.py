0f3a834c2beac238e12415f192f11404dc18c707
Yen900611 <yen900611@gmail.com>
2020-06-16 11:17:31 +0800

    add COIN Mode

--- game/coinPlayMode.py
+++ game/coinPlayMode.py
@@ -8,6 +8,7 @@ class CoinPlayingMode(PlayingMode):
     def __init__(self, user_num):
         super(CoinPlayingMode, self).__init__(user_num)
         self.creat_coin_time = pygame.time.get_ticks()
+        self.coins = pygame.sprite.Group()
 
     def update_sprite(self,command:list):
         self.frame += 1
@@ -20,6 +21,7 @@ class CoinPlayingMode(PlayingMode):
 
         for car in self.user_cars:
             car.update(command[car.car_no])
+            self.collide_coins(car)
 
             '''是否通過終點'''
             self.is_car_arrive_end(car)
@@ -47,10 +49,36 @@ class CoinPlayingMode(PlayingMode):
 
     def creat_coins(self):
         if pygame.time.get_ticks() - self.creat_coin_time > 5000:
-            coin = Coin(random.choice(self.lane_center), 0, self.maxVel)
+            coin = Coin(random.choice(self.lane_center), 0)
             self.all_sprites.add(coin)
+            self.coins.add(coin)
             self.creat_coin_time = pygame.time.get_ticks()
         pass
 
     def collide_coins(self,car):
+        hits = pygame.sprite.spritecollide(car, self.coins, True)
+        for hit in hits:
+            car.coin_num += 1
         pass
+
+    def is_car_arrive_end(self, car):
+        if car.distance > self.end_line:
+            user_coins = []
+            for user in self.user_cars:
+                user_coins.append(user.coin_num)
+            for user in self.user_cars:
+                if user.coin_num == min(user_coins):
+                    user_coins.remove(user.coin_num)
+                    user.state = False
+                    self.detect_car_state(user)
+
+    def draw_user_imformation(self):
+        for car in self.user_cars:
+            self.draw_information(self.screen, "Player" + str(car.car_no+1) +
+                                  "("+USER_COLOR[car.car_no]+")", 17, 715, (car.car_no) * 120 + 10)
+            self.draw_information(self.screen, "vel : " + str(round(car.velocity, 2)), 17, 715,
+                                  (car.car_no) * 120 + 40)
+            self.draw_information(self.screen, "distance : " + str(abs(round(car.distance, 2))), 17, 715,
+                                  (car.car_no) * 120 + 70)
+            self.draw_information(self.screen, "coins : " + str(car.coin_num), 17, 715,
+                                  (car.car_no) * 120 + 100)
